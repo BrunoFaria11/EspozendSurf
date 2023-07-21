5a81ebeb8d91abe45b625a5089146583382e6fe4
BrunoFaria11 <bruno.eduardo.faria99@gmail.com>
2023-07-21 13:07:30 +0100

    test map

--- src/app/components/front-end/map/map.component.ts
+++ src/app/components/front-end/map/map.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { LanguageStoreService } from 'src/core/services/stores/language-store.service';
 
+declare function loadMap(markers): any;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
-
-  constructor(public languageStoreService: LanguageStoreService) { }
+  constructor(public languageStoreService: LanguageStoreService) {}
 
   ngOnInit(): void {
+    loadMap(this.languageStoreService.Texts.section6.countries);
   }
-
 }
---
+++ src/assets/front-end/map/js/main.js
@@ -0,0 +1,37 @@
+function loadMap(markers){
+  const map = new jsVectorMap({
+    map: "world", // 'canada', ...
+    selector: "#map",
+    backgroundColor: "tranparent",
+    draggable: true,
+    zoomButtons: false,
+    zoomOnScroll: true,
+    zoomOnScrollSpeed: 3,
+    zoomMax: 12,
+    zoomMin: 1,
+    zoomAnimate: true,
+    showTooltip: false,
+    zoomStep: 1.5,
+    regionsSelectable: false,
+    regionsSelectableOne: false,
+    regionStyle: {
+      // Region style
+      initial: {
+        fill: "#e3eaef",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 0,
+        strokeOpacity: 1,
+      },
+      hover: {
+        fillOpacity: 1,
+        cursor: "pointer",
+      },
+      selected: {
+        fill: "#000",
+      },
+      selectedHover: {},
+    },
+    markers: markers,
+  });
+};
